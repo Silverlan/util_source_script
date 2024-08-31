c4730a08a69dd891d1cf4b3aac6c25fde9fcd950
Silverlan <silverlann@gmail.com>
2024-08-31 20:27:03 +0200

    fix(clang): fix build error

--- src/implementation/scene.cpp
+++ src/implementation/scene.cpp
@@ -2,6 +2,8 @@
  * License, v. 2.0. If a copy of the MPL was not distributed with this
  * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
 
+module;
+
 #include <sharedutils/util_markup_file.hpp>
 #include <sharedutils/util.h>
 #include <array>
